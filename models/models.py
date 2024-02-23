9da0de9307075acf5f5513ba5f24e84bcbc69ea5
Ruben <rubencidep@gmail.com>
2024-02-23 18:27:13 +0100

    Modulo funcionando

--- models/models.py
+++ models/models.py
@@ -18,11 +18,12 @@ import base64
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
 from odoo import models, fields, api
 class Tournament(models.Model):
     _name = 'gametournament.tournament'
     name = fields.Char(string="Name", required=True, help="Nombre del torneo")
-    logo = fields.Image(string="Logo", help="Logo del torneo")
+    logo = fields.Image(string="Logo", help="Logo del torneo", maxheight=120)
     description = fields.Text()
     prize = fields.Float(string="Prize")
     start_date = fields.Date()
@@ -33,12 +34,41 @@ class Tournament(models.Model):
 
     inscription_id = fields.One2many('gametournament.inscription', 'inscription_id', string="Inscription")
 
+    game_id = fields.Many2many('gametournament.game','game_tournament_rel', 'tournament_id', 'game_id',ondelete='cascade', string="Game", auto_join=True)
+
 
 class Inscription(models.Model):
     _name = 'gametournament.inscription'
 
-    # name = fields.Char(string="Name Team or player", required=True)
-    name_id = fields.Many2one('res.partner', ondelete='set null', string="Name Team or player")
-    date_inscription = fields.Date()
+    name_id = fields.Many2many("res.partner", string='Name team or player')
+    name_display = fields.Char(string="Team or Player Names", compute='_compute_name_display')
 
-    inscription_id = fields.Many2one('gametournament.tournament', ondelete='cascade', string="Tournament", required=True)
+    date_inscription = fields.Date()
+    inscription_id = fields.Many2one('gametournament.tournament', ondelete='cascade', string="Tournament",
+                                     required=True)
+
+    @api.depends('name_id')
+    def _compute_name_display(self):
+        for record in self:
+            names = ", ".join(record.name_id.mapped('name'))
+            record.name_display = names
+
+
+class Game(models.Model):
+    _name = 'gametournament.game'
+
+    name = fields.Char(string="Name Game", required=True)
+    gender = fields.Char(string="Gender")
+    platform = fields.Char(string="Platform")
+    description = fields.Text()
+    # game_id = fields.Many2many('gametournament.tournament', ondelete='cascade', string='Tournament')
+    # games = fields.Char(string="Game Names", compute='_compute_name_display')
+    tournament_ids = fields.Many2many('gametournament.tournament', 'game_tournament_rel', 'game_id', 'tournament_id',string='Tournaments',auto_join=True)
+
+    tournament_names = fields.Char(ondelete='cascade',string="Tournament Names", compute='_compute_tournament_names')
+
+    @api.depends('tournament_ids')
+    def _compute_tournament_names(self):
+        for game in self:
+            tournament_names = ", ".join(game.tournament_ids.mapped('name'))
+            game.tournament_names = tournament_names
